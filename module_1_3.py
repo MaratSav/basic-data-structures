24dd2c5fc95eb96d774fc20742fb4df9db06a8ae
MaratSav <marat1620@gmail.com>
2024-11-03 19:39:36 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,15 @@
+name = 'Marat'
+print (name)
+age = 38
+print (age)
+new_age = 1
+print (age + new_age)
+is_student = age > new_age
+print (is_student)
+
+userName = 'Marat'
+userAge = 38
+userNew_age = age + 1
+userIs_student = age > new_age
+user = (f'\n Name: {userName} \n Age: {userAge} \n New_age: {userNew_age} \n Is_student: {userIs_student}')
+print (user)

f98930b5d454e9737d52c151556260c34dc5ce42
MaratSav <marat1620@gmail.com>
2024-11-04 18:41:03 +0300

    переменные

--- variables.py
+++ variables.py
@@ -3,10 +3,3 @@ hours_spent = 1.5
 course_name = 'Python'
 time_for_one_task = hours_spent / home_work
 print ( course_name, home_work, hours_spent, time_for_one_task)
-
-userHome_work = 12
-userHours_spent = 1.5
-userCourse_name = 'Python'
-userTime_for_one_task = hours_spent / home_work
-user = (f' Курс: {course_name} \n Всего задач: {home_work} \n Затрачено часов: {hours_spent} \n Время на одно задание: {time_for_one_task}')
-print (user)

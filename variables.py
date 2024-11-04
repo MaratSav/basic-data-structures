5febf3c1552f257559458efd3834b2db9dbd9953
MaratSav <marat1620@gmail.com>
2024-11-04 18:52:24 +0300

    переменные

--- variables.py
+++ variables.py
@@ -2,4 +2,4 @@ home_work = 12
 hours_spent = 1.5
 course_name = 'Python'
 time_for_one_task = hours_spent / home_work
-print ( course_name, home_work, hours_spent, time_for_one_task)
+print (course_name,  home_work, hours_spent, time_for_one_task)
